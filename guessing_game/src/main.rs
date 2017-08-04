2a52d3feadfaa65df0d42d2fbe2375df3fda45c4
Santiago Munoz <smr.rambar@gmail.com>
2017-08-04 10:25:18 +0200

    Fix

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,7 +8,6 @@ fn main() {
 
     let random = rand::thread_rng().gen_range(1,101);
 
-    println!("The secret number is {}", random);
     println!("Guess the number!");
    
     loop{
@@ -16,8 +15,10 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = guess.trim().parse()
-            .expect("Please input a number");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
         println!("You guessed {}",guess);
     
